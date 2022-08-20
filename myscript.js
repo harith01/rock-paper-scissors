d04e24e7d8cda61244ae05385a7d525b4fca4b30
harith01 <harithyusuph@gmail.com>
2022-08-20 16:28:21 +0100

    declaring playRound() function

--- myscript.js
+++ myscript.js
@@ -4,3 +4,22 @@ function getComputerChoice(){
     return selection
 }
 
+function playRound(playerSelection, computerSelection){
+    let outcome;
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
+        outcome = 'Tied!!!';
+    } else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'scissors'){
+        outcome = 'You Win! Rock blunts Scissors';
+    } else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'paper'){
+        outcome = 'You Lose! Paper Covers Rock';
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'scissors'){
+        outcome = 'You Lose! Scissors cuts Paper';
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'rock'){
+        outcome = 'You Win! Paper covers rock';
+    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'paper'){
+        outcome = 'You Win! Scissors cuts Paper';
+    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'rock'){
+        outcome = 'You Lose! Rock blunts Scissors';
+    }
+    return outcome
+}
