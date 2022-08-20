98db84145ff355464756b77f18af0ce1033b4933
harith01 <harithyusuph@gmail.com>
2022-08-20 17:40:14 +0100

    Declaration of game() Function
    The game function is required to run the playRound() function
    5 times and reports a winner or loser at the end
    
    - Modified the return value for playRound() function to 0,1 and -1
    for tied, win and lose respectively.
    - Inside the declaration statement of the game() function, 3 variables;
    win, lose and tied were created and initialized to 0.
    - created a loop to run 5 times.
    - Get choice of user from a prompt()
    - Generated the computer choice using getComputerChoice()
    - Called the playRound()
    - Based on the returned value of the playRound(). the appropriate
    variable is incremented
    - Based on the state of the variables, a winner or losser is declared

--- myscript.js
+++ myscript.js
@@ -7,19 +7,57 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection){
     let outcome;
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
-        outcome = 'Tied!!!';
+        // outcome = 'Tied!!!';
+        outcome = 0;
     } else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'scissors'){
-        outcome = 'You Win! Rock blunts Scissors';
+        // outcome = 'You Win! Rock blunts Scissors';
+        outcome = 1
     } else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'paper'){
-        outcome = 'You Lose! Paper Covers Rock';
+        // outcome = 'You Lose! Paper Covers Rock';
+        outcome = -1;
     } else if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'scissors'){
-        outcome = 'You Lose! Scissors cuts Paper';
+        // outcome = 'You Lose! Scissors cuts Paper';
+        outcome = -1;
     } else if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'rock'){
-        outcome = 'You Win! Paper covers rock';
+        // outcome = 'You Win! Paper covers rock';
+        outcome = 1;
     } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'paper'){
-        outcome = 'You Win! Scissors cuts Paper';
+        // outcome = 'You Win! Scissors cuts Paper';
+        outcome = 1;
     } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'rock'){
-        outcome = 'You Lose! Rock blunts Scissors';
+        // outcome = 'You Lose! Rock blunts Scissors';
+        outcome = -1;
     }
     return outcome
+}
+
+function game(){
+    let win = 0;
+    let lose = 0;
+    let tie = 0;
+    for (let i = 0; i < 5; i++){
+        playerChoice = prompt('Choose Rock/Paper/Scissors');
+        computerChoice = getComputerChoice();
+        outcome = playRound(playerChoice, computerChoice);
+        switch (outcome){
+            case 0:
+                tie++;
+                console.log('Tied');
+            break;
+            case 1:
+                win++;
+                console.log('You Win!');
+            break;
+            case -1:
+                lose++;
+                console.log('You Lost!');
+        }
+    }
+    if (win > lose){
+        console.log('You are the Winner');
+    } else if (win == lose){
+        console.log('The game is Tied');
+    } else {
+        console.log('You are the loser');
+    }
 }
